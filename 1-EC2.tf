00a9b491e525eb9d4f701716e9b448b73e5fec26
elnana93 <e5leadsusa@gmail.com>
2026-01-20 17:19:47 -0800

    final version

--- 1-EC2.tf
+++ 1-EC2.tf
@@ -72,8 +72,6 @@ output "lab_ec2_instance_profile_name" {
 }
 
 
-
-
 output "lab_ec2_public_ip" {
   value = aws_instance.lab_ec2_app.public_ip
 }
@@ -86,3 +84,4 @@ output "lab_ec2_public_url" {
   value = "http://${coalesce(aws_instance.lab_ec2_app.public_dns, aws_instance.lab_ec2_app.public_ip)}"
 }
 
+# create an output for ssh command later
